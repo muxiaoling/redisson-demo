d2671c89f9a536c292791e18c70bd22d17b14539
muxiaoling <860079445@qq.com>
2022-09-14 10:15:40 +0800

    第一次提交

---
+++ src/main/java/com/example/redisson/config/RedissonConfig.java
@@ -0,0 +1,23 @@
+package com.example.redisson.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+    @Autowired
+    private RedisProperties redisProperties;
+
+    @Bean
+    public RedissonClient redissonSingle() {
+        Config config = new Config();
+        String redisUrl = String.format("redis://%s:%s", redisProperties.getHost() + "", redisProperties.getPort() + "");
+        config.useSingleServer().setAddress(redisUrl).setPassword(redisProperties.getPassword());
+        return Redisson.create(config);
+    }
+}
---
+++ src/main/java/com/example/redisson/util/RedissonLockUtil.java
@@ -0,0 +1,6 @@
+package com.example.redisson.util;
+
+
+public class RedissonLockUtil {
+
+}
